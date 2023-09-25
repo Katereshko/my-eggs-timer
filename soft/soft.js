e96df7e1f7e554108362c70b2f63831dfcc16b42
Katerina <ekaterina.tereshko.me@gmail.com>
2023-09-25 13:55:05 +0300

    first commit

---
+++ soft/soft.js
@@ -0,0 +1,44 @@
+const timer = 4; 
+let amountTime = timer * 60;
+
+const start = document.querySelector('#startCountdown');
+start.addEventListener('click', calculateTime);
+let timerID;
+let a = 0;
+
+function calculateTime(){
+    
+    const countdown = document.querySelector('#countdown');
+    let minutes = Math.floor(amountTime / 60);
+    let seconds = amountTime%60;
+
+    if (seconds < 10){
+        seconds = "0" + seconds;
+    }
+
+    countdown.textContent = `${minutes} : ${seconds}`;
+    amountTime--;
+    a = a+0.4167;
+    const percentage = document.querySelector('.percentage');
+    percentage.style.width = a+'%';
+
+    timerID = setTimeout(calculateTime, 1000);
+    console.log(amountTime);
+
+    if(amountTime < 0){
+        amountTime = 0;
+        stopTimer();
+        timeoutSound();
+    }
+
+}
+
+function stopTimer(){
+    clearTimeout(timerID);
+}
+
+function timeoutSound() {
+    var audio = new Audio();
+    audio.src = 'budilnik.mp3';
+    audio.autoplay = true;
+}
